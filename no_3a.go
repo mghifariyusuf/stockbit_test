7e16929c904276604e2060829a99bee875483d7f
ghifari.yusuf <ghifari.yusuf@mola.tv>
2021-01-20 15:34:07 +0700

    add no_3a.go

---
+++ no_3a.go
@@ -0,0 +1,12 @@
+func findFirstStringInBracket(s string) string {
+	i := strings.IndexByte(s, '(')
+	if i < 0 {
+		return ""
+	}
+	i++
+	j := strings.IndexByte(s[i:], ')')
+	if j < 0 {
+		return ""
+	}
+	return s[i : i+j]
+}
